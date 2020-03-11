3b580fa754da46e2dc88b21fc639b8128f39a3c0
wangwh01 <wangwh01@fenbi.com>
2020-03-11 11:16:13 +0800

    MOD: 优化感谢老师

---
+++ miniprogram/pages/social/components/teacher-card/app-teacher-card.js
@@ -0,0 +1,26 @@
+// pages/social/components/teacher-card/app-teacher-card.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        bgColor: {
+            type: String,
+            value: ''
+        },
+        socialData: {
+            type: Object,
+            value: {}
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {},
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {}
+})
