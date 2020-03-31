b35632c253bd8cb07058ee07c765408ea950635e
wangwh01 <wangwh01@fenbi.com>
2020-03-31 23:48:21 +0800

    ADD: 新首页

---
+++ miniprogram/pages/schedule/pages/new-home/app-new-home.js
@@ -0,0 +1,19 @@
+// pages/schedule/pages/new-home/app-new-home.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {},
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        roomList: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {}
+})
